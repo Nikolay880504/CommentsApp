77c1063e267413c8cda0bc79e1e5e06e8a5a96ff
NIKOLAY <stin541988@gmail.com>
2025-10-25 19:55:06 +0300

    Initial commit: ASP.NET Core API and Angular SPA setup

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+namespace CommentsApp
+    
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            app.UseStaticFiles();
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (app.Environment.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+            app.Run();
+        }
+    }
+}
